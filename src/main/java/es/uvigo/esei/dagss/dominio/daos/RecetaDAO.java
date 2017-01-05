d8fbac0009e5fcee59102b843fbf5f7589ef5bd7
cyanide4all <marvazto@gmail.com>
2017-01-05 18:02:13 +0100

    HDU 2 de Farmacia acabada

--- src/main/java/es/uvigo/esei/dagss/dominio/daos/RecetaDAO.java
+++ src/main/java/es/uvigo/esei/dagss/dominio/daos/RecetaDAO.java
@@ -4,13 +4,26 @@
 
 package es.uvigo.esei.dagss.dominio.daos;
 
+import es.uvigo.esei.dagss.dominio.entidades.Cita;
+import es.uvigo.esei.dagss.dominio.entidades.Medico;
 import es.uvigo.esei.dagss.dominio.entidades.Receta;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 @Stateless
 @LocalBean
 public class RecetaDAO extends GenericoDAO<Receta>{
- 
-    // Completar aqui
+    public List<Receta> getRecetasActuales(String nTarjeta){
+        Date fecha = Calendar.getInstance().getTime(); //TODO usar a partir de una fecha a mayores
+        TypedQuery<Receta> tq = em.createQuery("SELECT r FROM Receta AS r"
+                + " WHERE r.prescripcion.paciente.numeroTarjetaSanitaria = :ab AND :fecha BETWEEN r.inicioValidez AND r.finValidez", Receta.class); 
+        tq.setParameter("ab", nTarjeta);
+        tq.setParameter("fecha", fecha);
+        List<Receta> toret = tq.getResultList();
+        return toret;
+    }
 }
